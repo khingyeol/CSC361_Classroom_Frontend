3cd955cb1c3be903ed400254e0f572bcad2b8ebb
jojjoeeeeeee <jo-jo_kiki@hotmail.com>
2021-11-11 01:07:10 +0700

    authen

--- src/components/main/AuthCard.js
+++ src/components/main/AuthCard.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { login } from '../../services/actions/api_auth'
 import '../../index.css'
 
 const AuthCard = () => {
+    const [inputs, setInputs] = useState({})
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(values => ({...values, [name]: value}))
+      }
+
+    const onTappedLogin = async (e) => {
+        e.preventDefault();
+        const res = await login(inputs);
+        if(res.auth){
+            console.log(res.data);
+        }
+        else {
+            console.log(res.data);
+        }
+    };
+
 return (
     <div>
         <div className="authbox box shadow-lg align-bottom">
             <div className="eiei">
-                <form>
+                <form onSubmit={onTappedLogin}>
                 <h1 className="">Loginsdfsdsssssssssssssssssssssssssss</h1>
-                <input type="text" className="tf" placeholder="Username"></input>
-                <input type="text" className="tf" placeholder="Password"></input>
+                <input 
+                type="text" 
+                className="tf" 
+                placeholder="Username" 
+                name="username"
+                value={inputs.username || ""} 
+                onChange={handleChange}
+                />
+                <input 
+                type="text" 
+                className="tf" 
+                placeholder="Password"
+                name="password"
+                value={inputs.password || ""} 
+                onChange={handleChange}
+                />
                 <input type="submit" className="btn"></input>
                 </form>
             </div>
