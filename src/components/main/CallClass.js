200dec0fc091cdf9195dfebbe5157f443708ee8b
jojjoeeeeeee <jo-jo_kiki@hotmail.com>
2021-11-10 23:50:04 +0700

    services

--- src/components/main/CallClass.js
+++ src/components/main/CallClass.js
@@ -1,14 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getAllClass } from '../../services/actions/api_class'
 // import axios from 'axios'
 import '../../index.css'
 
 function CallClass(){
-    const content = useState([])
+    const [content, setContent] = useState([])
 
-    const classlist = content.map(() =>
-        <div className="class-list">
-            ei
-            <div className="sub">B01</div>
+
+    useEffect( async () => {
+        const res = await getAllClass();
+        if (res.data.result = 'OK') {
+            setContent(res.data.data)
+        }
+        else {
+            setContent([])
+        }
+        
+    }, []);
+
+    const classlist = content.map((item) =>
+        <div key="{item.class_code}" className="class-list">
+            { item.class_name}
+            <div className="sub">{ item.class_section}</div>
         </div>
         
     ) 
